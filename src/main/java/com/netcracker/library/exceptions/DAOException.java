eb49b0d274a88b6b30f6c415df11b0a96f284417
Daniil Harapko <dharapko@gmail.com>
2016-11-17 21:09:38 +0300

    Removed BookException. Added DAOException.

---
+++ src/main/java/com/netcracker/library/exceptions/DAOException.java
@@ -0,0 +1,10 @@
+package com.netcracker.library.exceptions;
+
+/**
+ * Created by raumo0 on 17.11.16.
+ */
+public class DAOException extends Exception {
+    public DAOException(String msg) {
+        super(msg);
+    }
+}
