568e375e59ecd7a16dcbf8b4583f01fcff8ea4cf
Daniil Harapko <dharapko@gmail.com>
2016-11-17 16:05:48 +0300

    Removed menu.

--- src/main/java/com/netcracker/library/menu/command/login/librarian/LibrarianCommand.java
+++
@@ -1,9 +0,0 @@
-package com.netcracker.library.menu.command.login.librarian;
-
-import com.netcracker.library.menu.command.Command;
-
-/**
- * Created by raumo0 on 31.10.16.
- */
-public interface LibrarianCommand extends Command {
-}
--- src/main/java/com/netcracker/library/menu/command/login/reader/ReaderCommand.java
+++
@@ -1,9 +0,0 @@
-package com.netcracker.library.menu.command.login.reader;
-
-import com.netcracker.library.menu.command.Command;
-
-/**
- * Created by raumo0 on 30.10.16.
- */
-public interface ReaderCommand extends Command {
-}
