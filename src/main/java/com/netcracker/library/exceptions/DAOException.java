9f97d0105429974eb1b89d51424b7fc849a75ea5
Daniil Harapko <dharapko@gmail.com>
2016-11-18 15:27:24 +0300

    Added DAOException constructors.

--- src/main/java/com/netcracker/library/exceptions/DAOException.java
+++ src/main/java/com/netcracker/library/exceptions/DAOException.java
@@ -4,7 +4,19 @@ package com.netcracker.library.exceptions;
  * Created by raumo0 on 17.11.16.
  */
 public class DAOException extends Exception {
+    public DAOException() {
+        super();
+    }
+
     public DAOException(String msg) {
         super(msg);
+    }
+
+    public DAOException(Throwable cause) {
+        super(cause);
+    }
+
+    public DAOException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
